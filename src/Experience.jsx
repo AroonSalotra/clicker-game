fb200362163e4c6d13d0641126707b553775fead
Aroon Salotra <aroonsalotra@gmail.com>
2022-09-11 17:22:10 +0100

    exp now scales per level

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,23 +3,26 @@ import Bar from "./Bar";
 
 const Experience = ({ enemies, goldModifier, setGoldModifier, experience, setExperience }) => {
     const [level, setLevel] = useState(1)
-
-    const handleLevel = () => {
-
-    }
+    const [expCap, setExpCap] = useState(99)
+    const [expPercent, setExpPercent] = useState(null)
 
     useEffect(() => {
-        if (experience > 99) {
+        
+        setExpPercent(Math.round(experience / expCap * 99))
+
+        if (experience > expCap) {
             setLevel(level + 1)
             setExperience(0)
             setGoldModifier(level / 2.5)
-        }
+            setExpCap(expCap + 20)
+        } 
     }, [experience])
+
 
     return (
         <>
             <p className="level">Level: {level}</p>
-            <Bar completed={experience} />
+            <Bar completed={expPercent} />
         </>
     );
 }
