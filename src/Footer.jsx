b031f988b1d8bdb4992f5250712cd51643b0633d
Aroon Salotra <aroonsalotra@gmail.com>
2022-09-14 00:49:08 +0100

    footer now returns as data map

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,16 +1,29 @@
 const Footer = () => {
+
+    const Links = []
+
+    Links.push({
+        url: "https://aroonsalotra.github.io/portfolio-website/",
+        text: "My Website"
+    })
+
+    Links.push({
+        url: "https://github.com/AroonSalotra/clicker-game",
+        text: "Github Repo"
+    })
+    
     return (
         <footer className="footer">
             <ul>
-                <a href="#" target={"_blank"}>
-                    <li>My Website</li>
-                </a>
-                {/* <a href="#" target={"_blank"}>
-                    <li>Art by Beeffan</li>
-                </a> */}
-                <a href="#" target={"_blank"}>
-                    <li>Github Repo</li>
-                </a>
+                {Links.map(({ url, text }) => {
+                    return <a href={url}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={text}>
+
+                        <li>{text}</li>
+                    </a>
+                })}
             </ul>
         </footer>
     );
